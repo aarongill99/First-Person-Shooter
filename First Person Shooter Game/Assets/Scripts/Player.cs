3b486ccf375bb0c3bf1ffdb85f7773fe753254ee
aarongill99 <55856823+aarongill99@users.noreply.github.com>
2019-10-22 21:50:49 -0700

    Added Weapon
    Added
    -Firing weapon
    -Crosshair
    -Firing Animation
    -Hit Marker
    -Ammo Display
    -Reload

--- First Person Shooter Game/Assets/Scripts/Player.cs
+++ First Person Shooter Game/Assets/Scripts/Player.cs
@@ -9,18 +9,70 @@ public class Player : MonoBehaviour {
     private float _speed = 3.5f;
     [SerializeField]
     private float _gravity = 9.81f;
+    [SerializeField]
+    private GameObject _muzzleFlash;
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
+    [SerializeField]
+    private AudioSource _weaponSound;
+    [SerializeField]
+    private int _currentAmmo;
+    private int maxAmmo = 150;
+    [SerializeField]
+    private bool _isReloading = false;
 
+    private UIManager _uIManager;
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _currentAmmo = maxAmmo;
+        _uIManager = GameObject.Find("Canvas").GetComponent<UIManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if (Input.GetMouseButton(0) && _currentAmmo > 0 && _isReloading == false)
+        {
+            Shoot();
+        }
+        else
+        {
+            _muzzleFlash.SetActive(false);
+            _weaponSound.Stop();
+        }
 
+        if (Input.GetKeyDown(KeyCode.R) && _isReloading == false)
+        {
+            _isReloading = true;
+            StartCoroutine(Reload());
+        }
+        LockedMouse();
         CalculateMovement();
+    }
+
+    void Shoot()
+    {
+        _muzzleFlash.SetActive(true);
+        _currentAmmo--;
+        _uIManager.UpdateAmmo(_currentAmmo);
+        if (_weaponSound.isPlaying == false)
+        {
+            _weaponSound.Play();
+        }
+        Ray rayOrgin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(rayOrgin, out hitInfo))
+        {
+            Debug.Log("Hit: " + hitInfo.transform.name);
+            GameObject hitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+            Destroy(hitMarker, 1f);
+        }
     }
 
     void CalculateMovement()
@@ -32,5 +84,28 @@ public class Player : MonoBehaviour {
         velocity.y -= _gravity;
         velocity = transform.transform.TransformDirection(velocity);
         _controller.Move(velocity* Time.deltaTime);
+    }
+
+    void LockedMouse()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.5f);
+        _currentAmmo = maxAmmo;
+        _uIManager.UpdateAmmo(_currentAmmo);
+        _isReloading = false;
     }
 }
